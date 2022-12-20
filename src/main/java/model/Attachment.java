de74c66a4686890d3aa61e606c4f9c6f13eacc67
Shodiyorbek <shodiyorbektolqinov7777@gmail.com>
2022-12-20 11:30:58 +0500

    shodiyorbekdan 1-commit

--- src/main/java/model/Attachment.java
+++ src/main/java/model/Attachment.java
@@ -4,15 +4,37 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import model.base.BaseModel;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
 @Setter
-public class Attachment {
+public class Attachment extends BaseModel {
     private long id;
     private long size;
     private long attachmentId;
     private String contentType;
     private String fileName;
+
+    public Attachment(ResultSet resultSet) {
+    this.getList(resultSet);
+    }
+
+
+    @Override
+    protected void getList(ResultSet resultSet) {
+        try {
+            this.id = resultSet.getLong("id");
+            this.size = resultSet.getLong("size");
+            this.attachmentId = resultSet.getLong("attachment_id");
+            this.contentType = resultSet.getString("content_ty");
+            this.fileName = resultSet.getString("file_name");
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
