3727f97b51cacd21f007178da028e0199f7f2759
Akhror Mamasodirov <axror19977@gmail.com>
2022-12-21 09:35:15 +0500

    beauty still

--- src/main/java/dao/QuestionDatabase.java
+++ src/main/java/dao/QuestionDatabase.java
@@ -5,6 +5,7 @@ import model.Problem;
 import model.Attachment;
 import model.AttachmentContent;
 import model.Question;
+
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -14,7 +15,7 @@ import java.util.List;
 import java.util.Arrays;
 import java.util.List;
 
-public class QuestionDatabase extends BaseDatabaseConnection implements BaseDatabase<Question>{
+public class QuestionDatabase extends BaseDatabaseConnection implements BaseDatabase<Question> {
 
     @Override
     public boolean addObject(Question question) {
@@ -26,7 +27,7 @@ public class QuestionDatabase extends BaseDatabaseConnection implements BaseData
         return null;
     }
 
-    public boolean addObjectStange(Question question,AttachmentContent attachmentContent, Attachment attachment) {
+    public boolean addObjectStange(Question question, AttachmentContent attachmentContent, Attachment attachment) {
 
         Connection connection = null;
         PreparedStatement statement = null;
@@ -34,29 +35,29 @@ public class QuestionDatabase extends BaseDatabaseConnection implements BaseData
             connection = getConnection();
             statement = connection.prepareStatement("select * from add_question(?,?,?,?,?,?,?)");
             statement.setInt(1, question.getProblemId());
-            statement.setString(2,question.getType());
-            statement.setInt(3,question.getOrder());
-            statement.setBytes(4,attachmentContent.getContent());
-            statement.setString(5,question.getDescription());
-            statement.setString(6,question.getType());
-            statement.setLong(7,attachment.getSize());
+            statement.setString(2, question.getType());
+            statement.setInt(3, question.getOrder());
+            statement.setBytes(4, attachmentContent.getContent());
+            statement.setString(5, question.getDescription());
+            statement.setString(6, question.getType());
+            statement.setLong(7, attachment.getSize());
 
             ResultSet resultSet = statement.executeQuery();
 
-                return resultSet.next();
-            } catch (SQLException ex) {
+            return resultSet.next();
+        } catch (SQLException ex) {
             throw new RuntimeException(ex);
 
-    } finally {
-            closeConnection(connection,statement);
+        } finally {
+            closeConnection(connection, statement);
         }
 
     }
 
-  
+
     public List<Question> getObjectList(Integer questionId) {
-        try(Connection connection = getConnection();
-            PreparedStatement preparedStatement = connection.prepareStatement("select * from get_question(?)")) {
+        try (Connection connection = getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement("select * from get_question(?)")) {
 
             preparedStatement.setInt(1, questionId);
 
