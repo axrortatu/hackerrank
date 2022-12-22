e1779b2c1d2b578c16efacd5215e5ba1b18bc381
Akhror Mamasodirov <axror19977@gmail.com>
2022-12-22 12:19:53 +0500

    some changes

--- src/main/java/bot/BotDifficultyLanguange.java
+++ src/main/java/bot/BotDifficultyLanguange.java
@@ -14,9 +14,9 @@ public class BotDifficultyLanguange {
 
     public List<String> getThemeType(Long CHAT_ID) {
         List<String> themeType = new ArrayList<>();
-        if (botLang.getLanguangeName(CHAT_ID).equals(ENG)) {
+        if (botLang.getLanguageName(CHAT_ID).equals(ENG)) {
             themeType.addAll(List.of(TOPIC, PREPARATION, BACK));
-        } else if (botLang.getLanguangeName(CHAT_ID).equals(UZB)) {
+        } else if (botLang.getLanguageName(CHAT_ID).equals(UZB)) {
             themeType.addAll(List.of(TOPIC_UZB, PREPARATION_UZB, BACK_UZB));
         } else {
             themeType.addAll(List.of(TOPIC_RUS, PREPARATION_RUS, BACK_RUS));
@@ -26,9 +26,9 @@ public class BotDifficultyLanguange {
 
     public List<Object> getDifficultyType(Long CHAT_ID, int topicId) {
         List<String> difficultyLang = new ArrayList<>();
-        if (botLang.getLanguangeName(CHAT_ID).equals(ENG)) {
+        if (botLang.getLanguageName(CHAT_ID).equals(ENG)) {
             difficultyLang.addAll(List.of(EASY_ENG, MEDIUM_ENG, HARD_ENG, ALL_ENG));
-        } else if (botLang.getLanguangeName(CHAT_ID).equals(UZB)) {
+        } else if (botLang.getLanguageName(CHAT_ID).equals(UZB)) {
             difficultyLang.addAll(List.of(EASY_UZB, MEDIUM_UZB, HARD_UZB, ALL_UZB));
 
         } else {
